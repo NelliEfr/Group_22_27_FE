f834827eeaea097457135f213c000f767c206eca
Nelli Efremyan <n.efremyan@gmail.com>
2022-08-17 23:58:58 +0400

    modal form added

--- Lesson_17_nav_menu/scripts/script.js
+++ Lesson_17_nav_menu/scripts/script.js
@@ -1,6 +1,26 @@
+// Навигационное меню
+
 const nav_menu_btn = document.querySelector('.nav_menu_button');
 const nav_menu = document.querySelector('.nav_menu');
 
 nav_menu_btn.addEventListener('click', () => {
   nav_menu.classList.toggle('active')
-})
+});
+
+
+// Модальное окно
+
+const account_elem = document.querySelector('.account');
+
+const cross_elem = document.querySelector('.form_cross');
+
+const popup_elem = document.querySelector('.popup');
+
+
+account_elem.addEventListener('click', () => {
+  popup_elem.classList.add('visible')
+});
+
+cross_elem.addEventListener('click', () => {
+  popup_elem.classList.remove('visible')
+});
