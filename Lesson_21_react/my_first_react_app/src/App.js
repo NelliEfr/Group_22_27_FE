f9437426518550cf9825c2b9c3c3c3a291d22033
Nelli Efremyan <n.efremyan@gmail.com>
2022-09-15 00:03:10 +0400

    react started

---
+++ Lesson_21_react/my_first_react_app/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Заголовок</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti laborum perspiciatis, similique temporibus earum iure magnam ipsam. Facere at nostrum omnis ab, molestias cupiditate dolore debitis saepe praesentium libero provident!</p>
+      <img src="https://icdn.lenta.ru/images/2022/06/13/19/20220613193919054/pwa_list_rect_768_webp_84b752f857006144368eb465a9138a10.webp" alt="слон" />
+    </div>
+  );
+}
+
+export default App;
