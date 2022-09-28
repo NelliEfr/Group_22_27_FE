a7a404a2d715bb35a1413387eec2d8a865292031
Nelli Efremyan <n.efremyan@gmail.com>
2022-09-28 23:56:25 +0400

    triggers component added

--- Lesson_23_Vocabulary_cards/vocabulary_cards/src/components/App/index.jsx
+++ Lesson_23_Vocabulary_cards/vocabulary_cards/src/components/App/index.jsx
@@ -1,10 +1,12 @@
 import { words } from '../../data/words'
 import CardsContainer from '../CardsContainer'
+import Triggers from '../Triggers';
 
 function App() {
   return (
     <div>
       <CardsContainer cards={words} />
+      <Triggers />
     </div>
   );
 }
---
+++ Lesson_23_Vocabulary_cards/vocabulary_cards/src/components/Triggers/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import s from './index.module.css'
+
+export default function Triggers() {
+  return (
+    <div className={s.triggers}>
+      <button>Eng</button>
+      <button>Rus</button>
+    </div>
+  )
+}
