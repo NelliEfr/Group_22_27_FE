1a30f15239f3c0646bbe8180ed89061bebe67945
Nelli Efremyan <n.efremyan@gmail.com>
2022-10-05 23:45:36 +0400

    change to rus and ahange to eng added

--- Lesson_23_Vocabulary_cards/vocabulary_cards/src/components/App/index.jsx
+++ Lesson_23_Vocabulary_cards/vocabulary_cards/src/components/App/index.jsx
@@ -1,12 +1,33 @@
 import { words } from '../../data/words'
 import CardsContainer from '../CardsContainer'
 import Triggers from '../Triggers';
+import { useState } from 'react'
 
 function App() {
+  const [ cards, setCards ] = useState(words)
+
+  const change_to_rus = () => {
+    setCards(
+      cards.map(el => {
+        el.lang = 'rus';
+        return el
+      })
+    )
+  }
+
+  const change_to_eng = () => {
+    setCards(
+      cards.map(el => {
+        el.lang = 'eng';
+        return el
+      })
+    )
+  }
+
   return (
     <div>
-      <CardsContainer cards={words} />
-      <Triggers />
+      <CardsContainer cards={cards} />
+      <Triggers change_to_rus={change_to_rus} change_to_eng={change_to_eng} />
     </div>
   );
 }
--- Lesson_23_Vocabulary_cards/vocabulary_cards/src/components/Triggers/index.jsx
+++ Lesson_23_Vocabulary_cards/vocabulary_cards/src/components/Triggers/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import s from './index.module.css'
 
-export default function Triggers() {
+export default function Triggers({ change_to_rus, change_to_eng }) {
   return (
     <div className={s.triggers}>
-      <button>Eng</button>
-      <button>Rus</button>
+      <button onClick={change_to_eng}>Eng</button>
+      <button onClick={change_to_rus}>Rus</button>
     </div>
   )
 }
