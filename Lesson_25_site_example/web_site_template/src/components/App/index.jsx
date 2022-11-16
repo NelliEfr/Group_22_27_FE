db97e226d550c90f4453021c1e6652a7b62aa952
Nelli Efremyan <n.efremyan@gmail.com>
2022-11-16 23:00:59 +0400

    menu adaptive added

--- Lesson_25_site_example/web_site_template/src/components/App/index.jsx
+++ Lesson_25_site_example/web_site_template/src/components/App/index.jsx
@@ -4,11 +4,15 @@ import Contacts from '../Contacts';
 import Footer from '../Footer';
 import Navbar from '../Navbar';
 import Services from '../Services';
+import { useState } from 'react'
 
 function App() {
+
+  const [ menuActive, setMenuActive ] = useState(false);
+
   return (
     <div>
-     <Navbar />
+     <Navbar menuActive={menuActive} setMenuActive={setMenuActive} />
      <Services />
      <AboutUs />
      <Contacts />
--- Lesson_25_site_example/web_site_template/src/components/Navbar/index.jsx
+++ Lesson_25_site_example/web_site_template/src/components/Navbar/index.jsx
@@ -4,13 +4,13 @@ import s from './index.module.css'
 import { MenuOutlined } from '@ant-design/icons'
 import Button from '../Button'
 
-export default function Navbar() {
+export default function Navbar({ menuActive, setMenuActive}) {
   return (
     <nav className={['wrapper', s.nav_block].join(' ')}>
       <a href='#'>
         <img src={logo} alt='logo' />
       </a>
-      <ul className={s.nav_menu}>
+      <ul className={[s.nav_menu, menuActive ? s.active : ''].join(' ')}>
         <li>Главная</li>
         <li>Услуги</li>
         <li>Кейсы</li>
@@ -18,7 +18,7 @@ export default function Navbar() {
         <li>Контакты</li>
       </ul>
       <Button type='light_mode'>Связаться</Button>
-      <MenuOutlined className={s.menu_icon}/>
+      <MenuOutlined className={s.menu_icon} onClick={() => setMenuActive(menuActive = !menuActive)}/>
     </nav>
   )
 }
