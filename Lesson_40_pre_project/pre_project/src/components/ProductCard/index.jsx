705c79aa7a32a4ca0c22de4e812a4e99ba420c2b
Nelli Efremyan <n.efremyan@gmail.com>
2023-01-13 00:53:02 +0400

    product page finished

--- Lesson_40_pre_project/pre_project/src/components/ProductCard/index.jsx
+++ Lesson_40_pre_project/pre_project/src/components/ProductCard/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import s from './index.module.css'
+import { Link } from 'react-router-dom'
 
-export default function ProductCard({ title, image }) {
+export default function ProductCard({ id, title, image }) {
+
+  const product_url = `/product/${id}`
+
   return (
     <div className={s.product_card}>
-      <img src={image} alt={title} />
-      <p>{ title }</p>
+      <Link to={product_url}>
+        <img src={image} alt={title} />
+        <p>{ title }</p>
+      </Link>
       <button>Add to cart</button>
     </div>
   )
--- Lesson_40_pre_project/pre_project/src/pages/ProductPage/index.jsx
+++ Lesson_40_pre_project/pre_project/src/pages/ProductPage/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { load_product } from '../../requests/product_req';
 import { useDispatch, useSelector } from 'react-redux';
+import s from './index.module.css'
 
 export default function ProductPage() {
 
@@ -14,9 +15,21 @@ export default function ProductPage() {
     dispatch(load_product(id))
   }, []);
 
-  console.log(product);
+  const { title, description, price, image } = product;
 
   return (
-    <div>ProductPage {id}</div>
+    <div className={s.product_item}>
+      <img src={image} alt={title} />
+      <div className={s.info_block}>
+        <p className={s.title}>{title}</p>
+        <p className={s.description}>{description}</p>
+        <div className={s.price_block}>
+          <p>
+            <span>Price:</span> {price}
+          </p>
+          <button>Add to cart</button>
+        </div>
+      </div>
+    </div>
   )
 }
